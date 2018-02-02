5952a99115eafe695d35a00956251b4770c558fb
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2018-02-02 16:30:57 -0200

    :art: Update data model (#1)
    * Update data model
    
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>
    
    * Fix fk names
    
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

---
+++ src/apps/core/model_mixings.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+
+class AttributeMixing(models.Model):
+    field = models.CharField(max_length=200)
+    value = models.CharField(max_length=200)
+
+    class Meta:
+        verbose_name = _("Attribute Mixing")
+        verbose_name_plural = _("Attribute Mixings")
+        abstract = True
+
+    def __str__(self):
+        return '%s <%s>' % (self.field, self.value)
+
+
+class DomainAttributeMixing(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.CharField(max_length=150)
+    is_mandatory = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = _("Domain Attribute")
+        verbose_name_plural = _("Domain Attributes")
+        abstract = True
+
+    def __str__(self):
+        return self.name
