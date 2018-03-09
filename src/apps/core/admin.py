86949e1b627d148cc4ddad76e6c04f46c6ffc2f3
erivanio <erivanio.vanin@gmail.com>
2018-03-09 16:00:38 -0300

    :art: improve admin with list displays, filters, and search field

--- src/apps/core/admin.py
+++ src/apps/core/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.utils.html import format_html_join
+from django.utils.safestring import mark_safe
 from apps.core import models
 import nested_admin
 
@@ -34,6 +36,16 @@ class ManifestationTypeInline(nested_admin.NestedStackedInline):
 
 
 class ChannelAdmin(nested_admin.NestedModelAdmin):
+    list_display = ('name', 'manifestation_types')
+    search_fields = ('name', 'description')
+    ordering = ('name',)
+
+    def manifestation_types(self, instance):
+        return format_html_join(
+            mark_safe('<br/>'),
+            '{}',
+            ((mtype,) for mtype in instance.manifestation_types.all()),
+        )
 
     inlines = [
         ManifestationTypeInline,
@@ -48,6 +60,10 @@ class CollectAttributeInline(nested_admin.NestedStackedInline):
 
 
 class CollectAdmin(nested_admin.NestedModelAdmin):
+    list_display = ('channel', 'periodicity', 'initial_time', 'end_time')
+    list_filter = ('channel__name',)
+    search_fields = ('channel__name',)
+    ordering = ('channel__name',)
 
     inlines = [
         CollectAttributeInline,
@@ -60,6 +76,11 @@ class ProfileAttributeInline(nested_admin.NestedStackedInline):
 
 
 class ProfileAdmin(nested_admin.NestedModelAdmin):
+    list_display = ('author', 'id_in_channel', 'url', 'channel', 'is_reference')
+    list_filter = ('author__name', 'author__gender', 'channel__name',
+                   'is_reference')
+    search_fields = ('id_in_channel', 'url', 'channel__name', 'author__name')
+    ordering = ('author__name',)
 
     inlines = [
         ProfileAttributeInline,
@@ -72,10 +93,40 @@ class ManifestationAttributeInline(nested_admin.NestedStackedInline):
 
 
 class ManifestationAdmin(nested_admin.NestedModelAdmin):
-
+    list_display = ('short_content', 'manifestation_type', 'version', 'profile')
+    list_filter = ('manifestation_type__channel__name',
+                   'manifestation_type__name', 'profile__author',)
+    search_fields = ('id_in_channel', 'url', 'timestamp', 'content',
+                     'profile__author__name', 'manifestation_type__name',
+                     'manifestation_type__channel__name')
+    ordering = ('timestamp',)
     inlines = [
         ManifestationAttributeInline,
     ]
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'profiles', 'author_type', 'gender', 'birthdate',
+                    'cep')
+    list_filter = ('gender', 'birthdate')
+    search_fields = ('name', 'author_type', 'gender', 'cep')
+    ordering = ('name',)
+
+    def profiles(self, instance):
+        return format_html_join(
+            mark_safe('<br/>'),
+            '{}',
+            ((profile,) for profile in instance.profiles.all()),
+        )
+
+
+class CollectManifestationAdmin(admin.ModelAdmin):
+    list_display = ('collect', 'manifestation', 'timestamp')
+    list_filter = ('collect__channel__name',
+                   'manifestation__manifestation_type__name', 'timestamp')
+    search_fields = ('collect__channel__name', 'manifestation__content',
+                     'manifestation__manifestation_type__name')
+    ordering = ('timestamp',)
 
 
 class RelationshipProfileAttributeInline(nested_admin.NestedStackedInline):
@@ -84,6 +135,13 @@ class RelationshipProfileAttributeInline(nested_admin.NestedStackedInline):
 
 
 class RelationshipProfileAdmin(nested_admin.NestedModelAdmin):
+    list_display = ('profile', 'manifestation', 'relationship_type')
+    list_filter = ('profile__author__name', 'profile__channel__name',
+                   'manifestation__manifestation_type__name',
+                   'relationship_type')
+    search_fields = ('profile__author__name', 'profile__channel__name',
+                     'manifestation__manifestation_type__name')
+    ordering = ('profile__author__name',)
 
     inlines = [
         RelationshipProfileAttributeInline,
@@ -93,6 +151,7 @@ class RelationshipProfileAdmin(nested_admin.NestedModelAdmin):
 admin.site.register(models.Channel, ChannelAdmin)
 admin.site.register(models.Manifestation, ManifestationAdmin)
 admin.site.register(models.Profile, ProfileAdmin)
-admin.site.register(models.Author)
+admin.site.register(models.Author, AuthorAdmin)
 admin.site.register(models.Collect, CollectAdmin)
+admin.site.register(models.CollectManifestation, CollectManifestationAdmin)
 admin.site.register(models.RelationshipProfile, RelationshipProfileAdmin)
